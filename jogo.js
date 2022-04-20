01ec49764811784bbe88667c753be822b1d081f4
kamila <kamilapassos3836@gmail.com>
2022-04-20 11:45:24 -0300

    projeto app mata mosquito

--- jogo.js
+++ jogo.js
@@ -2,6 +2,8 @@
 
 var altura = 0
 var largura=0
+var vidas = 1
+ var tempo = 15
 function ajustaTamanhoPalcoJogo(){
    altura =window.innerHeight
    largura = window.innerWidth
@@ -9,9 +11,30 @@ function ajustaTamanhoPalcoJogo(){
 }
 ajustaTamanhoPalcoJogo()
 
-
+var cronometro = setInterval(function(){
+    tempo-=1
+    if(tempo<0){
+       clearInterval(cronometro) 
+       clearInterval(criaMosca)
+       window.location.href='vitoria.html'
+    }else{
+         document.getElementById('cronometro').innerHTML=tempo
+    }
+   
+},1000)
 
 function posicaoRandomica(){
+    if(document.getElementById('mosquito')){
+        document.getElementById('mosquito').remove()
+
+        if(vidas>3){
+           window.location.href = 'fim_de_jogo.html'
+        }else{
+              document.getElementById('v' +  vidas).src='img/coracao_vazio.png'
+        vidas++
+        }
+      
+    }
     
     var posicaoX=Math.floor( Math.random() * largura)-90
     var posicaoY=Math.floor( Math.random() * altura)-90
@@ -23,12 +46,17 @@ function posicaoRandomica(){
     //criando um elemento html
     var mosquito = document.createElement('img')
     mosquito.src='img/mosca.png'
-    mosquito.className=tamanhoAleatorio()
+    mosquito.className=tamanhoAleatorio() + ' ' + ladoAleatorio()
     mosquito.style.left=posicaoX+'px'
     mosquito.style.top=posicaoY+'px'
     mosquito.style.position='absolute'
+    mosquito.id='mosquito'
+    mosquito.onclick= function(){
+        this.remove()
+    }
     
     document.body.appendChild(mosquito)
+    ladoAleatorio()
     }
 
 
@@ -49,3 +77,16 @@ function posicaoRandomica(){
         }
     }
 
+
+
+    function ladoAleatorio(){
+        var classe =Math.floor( Math.random()*2 )
+        switch(classe){
+            case 0:
+            return 'ladoA'
+
+            case 1:
+            return 'ladoB'
+
+        }
+    }
